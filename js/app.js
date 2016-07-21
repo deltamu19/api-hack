7434d8b0cb5b638684dc7ba9c3a4417426497185
deltamu19 <daryll.martinez@gmail.com>
2016-07-20 22:58:50 -0700

    ajax call for character and events endpoints

--- js/app.js
+++ js/app.js
@@ -1,46 +1,50 @@
 
 $(document).ready(function(){
-	$('.thumbnail').clone();
-	getRequest();
 
-	/*
 	$('#search-term').submit(function(e) {
 		e.preventDefault();
 		var input = $('#input').val();
 		getRequest(input);
+		document.getElementById('search-term').reset();
 	});
-	*/
+
 });
 
 var getRequest = function(query) {
-/*	var hero = "nameStartsWith=" + query;
-	var	key = "&apikey=8c6cedb7a4672c9ba429030f7378775c";
-*/
 	var timestamp = new Date().getTime();
 	var privatekey = '01d950df46aec69f91751240a20e16ee3263e23b';
 	var publickey = '8c6cedb7a4672c9ba429030f7378775c';
 	var md5 = $.md5(timestamp + privatekey + publickey);
+
 	$.ajax({
-		url:'http://gateway.marvel.com:80/v1/public/events?limit=100&apikey=' + publickey + '&hash=' + md5 + '&ts=' + timestamp,
+		url:'http://gateway.marvel.com:80/v1/public/characters?name=' + query + '&limit=1&apikey=' + publickey + '&hash=' + md5 + '&ts=' + timestamp,
 		dataType: 'json',
 		type: 'GET'
 	})
 	.done(function(result){
-		var src = "";
-		var listObjects = result.data.results;
-		$.each(listObjects, function(index, value) {
-			
-			src += '<img class="thumbnail" src="' + value.thumbnail.path + '.' + value.thumbnail.extension + '">'
-		});
+		var character = result.data.results[0].id;
 
-		$('#events').html(src);
+	
 		
-		/*
-		$.each(listObjects)
-		
-		var list = .getFields(result.data.results[0], 'thumbnail.path');
-		$('#events').text(list);
-		*/
+		$.ajax({
+			url:'http://gateway.marvel.com:80/v1/public/events?characters=' + character + '&limit=100&apikey=' + publickey + '&hash=' + md5 + '&ts=' + timestamp,
+			dataType: 'json',
+			type: 'GET'
+		})
+		.done(function(result){
+			var src = "";
+			var href = "";
+			var listObjects = result.data.results;
+			$.each(listObjects, function(index, value) {
+				
+				src += '<a target="_blank" href="' + value.urls[0].url + '"><img class="thumbnail" src="' + value.thumbnail.path + '.' + value.thumbnail.extension + '"></a>';
+
+			});
+
+			$('#events').html(src);
+			
+
+		});
 	});
 };
 
